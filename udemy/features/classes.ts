7fa727d93209242375b2164b1c9fd407f0a25687
Joana Correia <redbugmilk@gmail.com>
2021-03-22 21:56:05 +0000

    super and properties

--- udemy/features/classes.ts
+++ udemy/features/classes.ts
@@ -1,38 +1,60 @@
+// class Vehicle {
+
+//     color: string;
+
+//     constructor(color: string){
+//         this.color = color;
+//     }
+
+//     private honk():void{
+//         console.log("beep");
+
+//     }
+//     //if no modifier assume public
+//     honk2():void{
+//         console.log("beep");
+
+//     }
+
+//     protected honk3():void{
+//         console.log("beep");
+
+//     }
+// }
+
 class Vehicle {
-    //if no modifier assume public
-    
-    private honk():void{
-        console.log("beep");
-
-    }
-
-    honk2():void{
-        console.log("beep");
-
-    }
-
-    protected honk3():void{
-        console.log("beep");
-
-    }
+  //can be private and protected too
+  constructor(public color: string) {}
 }
 
-class Car extends Vehicle{
-    private drive():void{
-        console.log("brumm brumm")
-    }
-    startDrivingProcess(): void {
-        this.drive();
-        this.honk3();
-    }
+class Car extends Vehicle {
+    //don't add public to color this is defined by the super
+  constructor(public wheels: number, color: string) {
+      super(color);
+  }
+  private drive(): void {
+    console.log("brumm brumm");
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    // this.honk3();
+  }
 }
 
-//instance
-const vehicle = new Vehicle();
-//vehicle.drive();
-vehicle.honk2();
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
 
-const car = new Car()
-car.honk2();
-//car.drive();
-car.startDrivingProcess();
+// //instance
+// const vehicle = new Vehicle();
+// console.log(vehicle.color)
+// //vehicle.drive();
+// vehicle.honk2();
+
+const car = new Car(4,"red");
+console.log(car.color);
+console.log(car.wheels);
+
+
+// car.honk2();
+// //car.drive();
+// car.startDrivingProcess();
