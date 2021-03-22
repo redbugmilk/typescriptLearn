dce1d0d53e1a991c47beb78403a9deaa26fe0c04
Joana Correia <redbugmilk@gmail.com>
2021-03-22 21:32:50 +0000

    modifiers

--- udemy/features/classes.ts
+++ udemy/features/classes.ts
@@ -1,24 +1,38 @@
 class Vehicle {
-    drive():void{
-        console.log("drive drive");
+    //if no modifier assume public
+    
+    private honk():void{
+        console.log("beep");
+
     }
-    honk():void{
+
+    honk2():void{
+        console.log("beep");
+
+    }
+
+    protected honk3():void{
         console.log("beep");
 
     }
 }
 
 class Car extends Vehicle{
-    drive():void{
+    private drive():void{
         console.log("brumm brumm")
+    }
+    startDrivingProcess(): void {
+        this.drive();
+        this.honk3();
     }
 }
 
 //instance
 const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+//vehicle.drive();
+vehicle.honk2();
 
 const car = new Car()
-car.honk();
-car.drive();
+car.honk2();
+//car.drive();
+car.startDrivingProcess();
