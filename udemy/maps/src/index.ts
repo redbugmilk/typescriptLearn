0862a20564e97ca1049d39e8f946f19b935ef2c7
Joana Correia <redbugmilk@gmail.com>
2021-03-30 22:49:14 +0100

    google maps working

--- udemy/maps/src/index.ts
+++ udemy/maps/src/index.ts
@@ -1,11 +1,15 @@
 console.log("hi there!");
 // import { User } from "./User";
-// import { Company } from "./Company";
+import { Company } from "./Company";
 
 // const user = new User();
 // console.log(user);
 
-// const company = new Company();
+const company = new Company();
 // console.log(company);
 
-google
+new google.maps.Map(document.getElementById("map"), {
+    clickableIcons: true,
+    zoom:1,
+    center: company.location
+});
