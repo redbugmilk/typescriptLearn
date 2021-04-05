145bcea01106242a6a38c8342b7b9924c3f08323
Joana Correia <redbugmilk@gmail.com>
2021-04-05 21:55:34 +0100

    refactor

--- udemy/sort/src/index.ts
+++ udemy/sort/src/index.ts
@@ -1,30 +1,4 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        //Wrong solution
-        //All this works if collection is number[]
-        //Type guard - other type of value
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        //All this works if collection is string
-        //Type guard - number string boolean symbol
-        if (typeof this.collection === "string") {
-          
-        }
-      }
-    }
-  }
-}
-
+import { Sorter } from "./Sorter";
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
