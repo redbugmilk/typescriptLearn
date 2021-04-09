9a5271c1aec61890cebb9e8661928da86b279c34
Joana Correia <redbugmilk@gmail.com>
2021-04-09 21:44:35 +0100

    change index to sort linked list

--- udemy/sort/src/index.ts
+++ udemy/sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -14,3 +15,14 @@ sorter.collection = charactersCollection;
 sorter.sort();
 console.log(sorter.collection);
 console.log(charactersCollection.data);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.add(25);
+linkedList.add(40);
+sorter.collection = linkedList;
+sorter.sort();
+linkedList.print();
+
