10d419817714f2040806c1848e6ab94a3221800d
Joana Correia <redbugmilk@gmail.com>
2021-07-13 22:26:27 +0100

    csv file reader

---
+++ udemy/stats/build/index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+csvFileReader.read();
+var matches = csvFileReader.data;
+console.log(csvFileReader.data);
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(manUnitedWins);
